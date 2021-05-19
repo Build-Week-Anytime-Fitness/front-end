6b805ac5512e11536c592b6a7b4869e42308bc4c
Chris Lau <chrislyc1991@gmail.com>
2021-05-18 23:32:50 -0600

    implemented LoginFormSchema for validation

---
+++ client/src/validation/schema.js
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+export const loginFormSchema = yup.object().shape({
+    userName:yup.string().required(),
+    email:yup.email().required(),
+});
+// ***
+// uncomment when working on the correct feature branches
+// ***
+// const signUpFormSchema = yup.object().shape({
+//     userName:yup.string().required(),
+//     email:yup.email().required(),
+//     password:yup.min(8,'password must have at least 8 characters').required(),
+//     firstName:yup.string().required(),
+//     lastName:yup.string().required(),
+//     birthday:yup.date().required()
+// });
+// const classFormSchema = yup.object().shape({
+//     name:yup.string().required(),
+//     type:yup.string().required(),
+//     startTime:yup.string().required(),
+//     duration:yup.number().positive().required(),
+//     intensityLevel:yup.string().required(),
+//     location:yup.string().required(),
+//     maxClassSize:yup.number().positive().integer().required()
+// });
