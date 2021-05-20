b9f1fe0802894cfd775be879e3a4666ad6553a74
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-05-19 23:40:39 -0700

    Fix conditional rendering of card. Find location with useLocation, flip boolean isInstructor, trigger conditional render

--- client/src/views/classes/Class.js
+++ client/src/views/classes/Class.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -12,10 +12,7 @@ import EditIcon from '@material-ui/icons/Edit';
 const useStyles = makeStyles({
   root: {
     minWidth: 310,
-    // color: 'white',
-    // background: 'black',
     opacity: .9,
-    // boxShadow: '0 0 25px red, 0 0 5px rgb(105, 7, 7)',
     fontSize: 22,
     margin: '10px',
   },
@@ -26,7 +23,6 @@ const useStyles = makeStyles({
   },
   title: {
     fontSize: 22,
-    // color: 'white',
   },
   pos: {
     marginBottom: 12,
@@ -44,8 +40,13 @@ export default function Class (props) {
   // console.log("classId from Class: ", classId); // gets the classId, A STRING
 
 
-  let { isInstructor } = useParams();
-  console.log("isInstructor from Class: ", isInstructor); // gets the isInstructor, A STRING
+  // Determines location from useLocation(), if "/instructors" is found, set isInstructor to true, trigger positive conditional render in card
+  let { pathname } = useLocation();
+  console.log("pathname from Class: ", pathname); // gets the location, looking for "/instructors", A STRING
+  let isInstructor = false;
+  if (pathname === "/instructors") {
+    isInstructor = true;
+  }
 
 
   // material UI code
