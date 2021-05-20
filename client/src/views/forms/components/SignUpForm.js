a43e057e24a447046ec3305c4b5df631539dc57a
Chris Lau <chrislyc1991@gmail.com>
2021-05-19 18:58:49 -0600

    added handleSubmit and modified checkbox element

--- client/src/views/forms/components/SignUpForm.js
+++ client/src/views/forms/components/SignUpForm.js
@@ -5,7 +5,7 @@ function LogInForm(props){
     // state variables
     const [isValid,setIsValid] = useState(true);
     // props variables
-    const {formValues,setFormValues,formErrors,setFormErrors} = props;
+    const {formValues,setFormValues,formErrors,setFormErrors,handleSubmit} = props;
     // useEffect
     useEffect(()=>{
         // validateForm whenever the component is mounted
@@ -14,13 +14,18 @@ function LogInForm(props){
     // function declarations
     const handleChange=(event)=>{
         const {name,value,checked,type} = event.target;
+        console.log(checked);
         const inputValue = type==='checkbox'?checked:value;
         validateField(signUpFormSchema,name,inputValue,formErrors,setFormErrors); //validate changed field using yup.reach
         validateForm(signUpFormSchema,formValues,setIsValid);
         setFormValues({...formValues,[name]:inputValue});
     };
+    const handleSubmitEvent=(event)=>{
+        event.preventDefault();
+        handleSubmit();
+    };
     return(
-        <div>
+        <form onSubmit={handleSubmitEvent}>
             <label>
                 Name
                 <input type='text' name='personName' value={formValues.personName} onChange={handleChange}></input>
@@ -35,17 +40,17 @@ function LogInForm(props){
             </label>
             <label>
                 Are you older than 18?
-                <input type='checkbox' name='isOverEighteen' value={formValues.isOverEighteen} onChange={handleChange}></input>
+                <input type='checkbox' name='isOverEighteen' checked={formValues.isOverEighteen} onChange={handleChange}></input>
             </label>
             <label>
                 Are you an instructor?
-                <input type='checkbox' name='isInstructor' value={formValues.isInstructor} onChange={handleChange}></input>
+                <input type='checkbox' name='isInstructor' checked={formValues.isInstructor} onChange={handleChange}></input>
             </label>
-            <button disabled={!isValid}>Log In</button>
+            <button type='submit' disabled={!isValid}>Log In</button>
             {
                 Object.keys(formErrors).map((key)=>formErrors[key]===''?'':<div>{formErrors[key]}</div>)
             }
-        </div>
+        </form>
     );
 }
 export default LogInForm;
