53e6a5de135cb8f96e41a7ab7a3570b98562c16d
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-05-20 18:33:29 -0700

    ClassForm added to /instructors view

--- client/src/views/Instructors.js
+++ client/src/views/Instructors.js
@@ -1,11 +1,12 @@
-import Classes from '../views/classes/Classes'
+import Classes from '../views/classes/Classes';
+import ClassForm from '../views/forms/components/ClassForm'
 
 export default function Instructors() {
 
     return (
         <>
         <h1>Instructors Page</h1>
-        {/* update form can go here */}
+        <ClassForm/>
         <Classes/>
         </>
     )
