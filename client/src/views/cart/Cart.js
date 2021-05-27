70814ad2b51a926de614f9976e4da52a8469f236
Chris Lau <chrislyc1991@gmail.com>
2021-05-26 20:30:48 -0600

    Finished logics for the Cart component

--- client/src/views/cart/Cart.js
+++ client/src/views/cart/Cart.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import CartItem from './CartItem';
 import {useHistory} from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 
 
 // this is for debugging purpose, please delete when redux is implemented
@@ -19,14 +20,8 @@ const Cart=()=>{
     // This state will be refactored into redux store
     // pull this from redux
     const [cartList,setCartList] = useState(exampleList);
-
     // history object
     const history = useHistory();
-
-    const handleClickCheckout=()=>{
-        // click event of the checkout button
-        history.push('/cart/checkout');
-    };
 
     const handleClickShopping=()=>{
         // click event of the continue shopping button
@@ -37,7 +32,7 @@ const Cart=()=>{
         // update to redux
         setCartList(cartList.filter((c)=>c.class_name!==class_name));
     };
-    
+
     const displayCartItems=(cartList)=>{
         if(cartList.length===0){
             return <div>Your cart is empty</div>;
@@ -46,13 +41,40 @@ const Cart=()=>{
             return cartList.map((c,i)=><CartItem key={i} indivClass={c} deleteCartItem={deleteCartItem}></CartItem>);
         }
     };
+    const makePayment = () => {
+        // const body = {
+        //   token,
+        //   product,
+        // }
+        // const header = {
+        //   'Content-Type': 'application/json',
+        // }
+        // return fetch(`http://localhost:8282/payment`, {
+        //   method: "POST",
+        //   headers: header,
+        //   body: JSON.stringify(body)
+        // }).then(response => {
+        //   console.log("RESPONSE ", response)
+        //   const { status } = response;
+        //   console.log("STATUS", status)
+        // })
+        //   .catch((err) => {
+        //     console.log(err)
+        //   })
+    }
+    const exampleAmount= 10;
     return (
     <div>
         <h1>Cart</h1>
+
         {displayCartItems(cartList)}
-        {/* <pre>{JSON.stringify(cartList)}</pre> */}
+
         <button onClick={handleClickShopping}>Continue shopping</button>
-        <button disabled={cartList.length===0} onClick={handleClickCheckout}>Checkout</button>
+
+        <StripeCheckout stripeKey="" token={makePayment} name="Anywhere Fitness" amount={exampleAmount*1000}>
+            <button disabled={cartList.length===0}>{`Checkout Total: $${exampleAmount}`}</button>
+        </StripeCheckout>
+        
         <div>delete me when footer is not covering me</div>
         <div>delete me when footer is not covering me</div>
     </div>
--- client/src/views/cart/CartItem.js
+++ client/src/views/cart/CartItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 const CartItem =(props)=>{
+
     const {class_name,class_date} = props.indivClass;
     const {deleteCartItem} = props;
     const handleClick=()=>{
         deleteCartItem(class_name);
     };
+    
     return (
         <div>
             <div>{class_name}</div>
