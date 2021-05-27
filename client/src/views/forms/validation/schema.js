34754583ed5ff16379b81c7a479ef4139d4cc77b
artofmayhem <chef_tony_miller@yahoo.com>
2021-05-26 18:30:57 -1000

    working final touches on crud function

--- client/src/views/forms/validation/schema.js
+++ client/src/views/forms/validation/schema.js
@@ -13,12 +13,12 @@ export const signUpFormSchema = yup.object().shape({
     is_instructor:yup.boolean()
 });
 export const classFormSchema = yup.object().shape({
-    className:yup.string().min(2,'Class name should have at least 2 characters').required(isRequired('Class name')),
-    classType:yup.string().required(isRequired('Class type')),
-    classDate:yup.string().required(isRequired('Class date')),
-    startTime:yup.string().required(isRequired('Start time')),
+    class_name:yup.string().min(2,'Class name should have at least 2 characters').required(isRequired('Class name')),
+    class_type:yup.string().required(isRequired('Class type')),
+    class_date:yup.string().required(isRequired('Class date')),
+    start_time:yup.string().required(isRequired('Start time')),
     duration:yup.number().moreThan(0.5,'Duration must be at leat 0.5 hour').required(isRequired('Duration')),
     intensity:yup.string().required(isRequired('Intensity')).oneOf(['low','medium','high'],'You must select an intensity'),
     location:yup.string().min(2,'Location must have at least 2 characters').required(isRequired('Location')),
-    maxClassSize:yup.number().moreThan(4,'Class size must be at least 5').integer().required(isRequired('CLass size'))
+    max_class_size:yup.number().moreThan(4,'Class size must be at least 5').integer().required(isRequired('CLass size'))
 });
