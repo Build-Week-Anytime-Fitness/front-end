9894801b8f2b0e81b416f9e37c3454ab1d3f7715
Chris Lau <chrislyc1991@gmail.com>
2021-06-02 22:57:23 -0600

    implemented makePayment

--- client/src/views/cart/Cart.js
+++ client/src/views/cart/Cart.js
@@ -2,13 +2,16 @@ import CartItem from './CartItem';
 import {useHistory} from 'react-router-dom';
 import StripeCheckout from 'react-stripe-checkout';
 import {connectToStore} from '../../state/interfaces/cartInterface';
+import axios from 'axios';
+require('dotenv').config();
+
 
 const Cart=(props)=>{
     // states: classes, user
     const {classes, myClasses} = props;
     // actions: payForClass(indivClass), undoSignUp(indivClass)
-    const {payForClass, undoSignUp} = props;
-
+    const {undoSignUp} = props;
+// payForClass
     const cartList = Object.keys(myClasses).filter((key)=>!myClasses[key].isPaid).map((key)=>classes[key]);
     // const cartList = [];
 
@@ -34,10 +37,38 @@ const Cart=(props)=>{
             return cartList.map((c,i)=><CartItem key={i} indivClass={c} deleteCartItem={deleteCartItem}></CartItem>);
         }
     };
-    const makePayment = () => {
-
+    const makePayment = (token) => {
+        // dummy data for testing
+        const fitnessClass = {
+            "id": 5,
+            "class_name": "Trip to Nirvana",
+            "class_type": "yoga + jazzersize",
+            "class_date": "Saturday",
+            "start_time": "7:00 am",
+            "duration": 1,
+            "intensity": "low",
+            "location": "park",
+            "number_of_students": 1,
+            "max_class_size": 40,
+            "instructor_id": 2
+          }
+        const body = {
+            token,
+            product: fitnessClass,
+          }
+          const headers = {
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJlbWFpbCI6ImJwQG1hcnZlbC5vcmciLCJpc19pbnN0cnVjdG9yIjp0cnVlLCJpYXQiOjE2MjIxMDY3MDAsImV4cCI6MTYyMjE5MzEwMH0.4TFdT6AGFe2rqunM30z7u0e5Kbps0qIwqxtPR5jAaZY',
+          }
+          return axios.create({headers}).post(`http://localhost:3000/payment`, body)
+          .then(response => {
+            console.log("RESPONSE ", response)
+            const { status } = response;
+            console.log("STATUS", status)
+          })
+          .catch((err) => {
+            console.log(err.stack)
+          })
     }
-    const exampleAmount= 10;
     return (
     <div>
         <h1>Cart</h1>
@@ -46,12 +77,13 @@ const Cart=(props)=>{
         
         <button onClick={handleClickShopping}>Continue shopping</button>
 
-        <StripeCheckout stripeKey="" token={makePayment} name="Anywhere Fitness" amount={exampleAmount*1000}>
-            <button disabled={cartList.length===0}>{`Checkout Total: $${exampleAmount}`}</button>
-        </StripeCheckout>
-        
-        <div>delete me when footer is not covering me</div>
-        <div>delete me when footer is not covering me</div>
+            {/*Stripe Secret Key needs to be stored in the .env file on the backend*/}
+            {/*Stripe Publishable Key needs to be stored in the .env file on the frontend - REACT_APP_KEY  environment variable stored on FE */}
+            {/*react-stripe-checkout package */}
+            {/*stripeKey prop will store the publishable key as process.env.REACT_APP_KEY*/}
+            <StripeCheckout stripeKey={process.env.REACT_APP_KEY} token={makePayment} name={'Anywhere Fitness Checkout'} amount={10.99 * 100}>
+                <button disabled={cartList.length===0}>Checkout</button>
+            </StripeCheckout>
     </div>
     );
 };
